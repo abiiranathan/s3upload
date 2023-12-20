56b9b82beffe2fdc94fcc599696d96558b5b1498
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2023-12-21 00:59:31 +0300

    Add CLI options for overriding access key, secret key and region

--- main.go
+++ main.go
@@ -17,12 +17,22 @@ var (
 	fileName   = ""
 	bucketName = ""
 	objectKey  = ""
+
+	// Authentication
+	accessKey = ""
+	secretKey = ""
+	region    = "eu-west-2"
 )
 
 func init() {
 	flag.StringVar(&fileName, "file", "", "File to upload to S3")
 	flag.StringVar(&bucketName, "bucket", "", "S3 bucket name")
 	flag.StringVar(&objectKey, "key", "", "Object key in S3")
+
+	flag.StringVar(&accessKey, "access-key", "", "AWS access key")
+	flag.StringVar(&secretKey, "secret-key", "", "AWS secret key")
+	flag.StringVar(&region, "region", region, "AWS region")
+
 	godotenv.Load()
 
 }
@@ -34,22 +44,26 @@ func main() {
 
 	// Check if required flags are provided
 	if fileName == "" || bucketName == "" || objectKey == "" {
-		fmt.Println("Usage: go run main.go -file <file_path> -bucket <bucket_name> -key <object_key>")
+		fmt.Println("Usage: go run main.go -file <file_path> -bucket <bucket_name> -key <object_key> -access-key <accessKey (Optional)> -secret-key <secret key(Optional)>")
 		os.Exit(1)
+	}
+
+	if accessKey == "" {
+		accessKey = os.Getenv("AWS_ACCESS_KEY")
+	}
+
+	if secretKey == "" {
+		secretKey = os.Getenv("AWS_SECRET_KEY")
 	}
 
 	// Create an S3 session
 	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String("eu-west-2"),
-		Credentials: credentials.NewStaticCredentials(
-			os.Getenv("AWS_ACCESS_KEY"),
-			os.Getenv("AWS_SECRET_KEY"),
-			"",
-		),
+		Region:      aws.String(region),
+		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
 	}))
 
 	// Create an S3 client
-	s3Client := s3.New(sess, aws.NewConfig().WithRegion("eu-west-2"))
+	s3Client := s3.New(sess, aws.NewConfig().WithRegion(region))
 
 	// Open the file
 	file, err := os.Open(fileName)
@@ -70,6 +84,5 @@ func main() {
 		fmt.Println("Error uploading file to S3:", err)
 		os.Exit(1)
 	}
-
 	fmt.Printf("File '%s' uploaded to S3 bucket '%s' with key '%s'\n", fileName, bucketName, objectKey)
 }
